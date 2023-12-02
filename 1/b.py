e414ef3751c9cba90fcba3b2db7ccbeead11b320
Brekig <42963322+Brekig@users.noreply.github.com>
2023-12-02 08:21:10 +0000

    some small bug missing, still incorrect

--- 1/b.py
+++ 1/b.py
@@ -1,9 +1,10 @@
 if __name__ == "__main__":
     dict_ = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", 
-         "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+         "six": "6", "seven": "7", "eight": "8", "nine": "9", "1": "1", "2": "2", 
+         "3": "3", "4": "4", "5": "5","6": "6", "7": "7", "8": "8", "9": "9"}
     sum = 0
     with open('./input.txt', 'r') as file:
-        for line in file:
+        for i, line in enumerate(file):
             line = line.strip()
             key = ""
             left = ""
@@ -12,7 +13,9 @@ if __name__ == "__main__":
             l = 0
             left_str = ""
             right_str = ""
-            while (l < r):
+            while (True):
+                if left and right:
+                    break
                 cl = line[l]
                 cr = line[r]
                 temp_l = left_str + cl
@@ -33,12 +36,7 @@ if __name__ == "__main__":
                     left = dict_[left_str]
                 if right_str in dict_ and right == "":
                     right = dict_[right_str]    
-                if cl.isdigit() and left == "":
-                    left = cl
-                if cr.isdigit() and right == "":
-                    right = cr
-                if left and right:
-                    break
+                
                 if left:
                     r -= 1
                 elif right:
@@ -46,11 +44,7 @@ if __name__ == "__main__":
                 else:
                     r -= 1
                     l += 1
-            print(left, right)
-            if not left:
-                left = "0"
-            if not right:
-                right = "0"
+            print("Line nr: ", i, " | ", left, right)
             sum += int(left + right)
     print(sum)
             
